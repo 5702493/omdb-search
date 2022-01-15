48bdbe7b8efceed3c9dc39bd5af36be2fd0282ec
Nikolai Kovachev <s0c14l@ya.ru>
2022-01-15 18:33:04 +0300

    add MovieComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [{
   path: '',
   pathMatch: 'full',
   component: HomeComponent
+}, {
+  path: 'movie/:id',
+  pathMatch: 'full',
+  component: MovieComponent
 }, {
   path: '**',
   redirectTo: '/'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
